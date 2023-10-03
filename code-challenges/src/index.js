fdf4abab55f7d86fdabeaa58c3e22d9d182804c3
Suleiman <suleosman73@gmail.com>
2023-10-03 11:42:47 +0300

    Added code to fetch data from the server

--- code-challenges/src/index.js
+++ code-challenges/src/index.js
@@ -1,1 +1,12 @@
 // Your code here
+//fetching the data from the server
+function fetchCharacters(){
+    fetch('http://localhost:3000/characters')
+    .then(Response => Response.json())
+    .then(characters => displayCharacterNames(characters));
+}
+
+function displayCharacterNames(characters){
+    const characterBar = document.getElementById('character-bar');
+
+}
